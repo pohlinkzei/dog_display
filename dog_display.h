8eb4fb26c5227f9e4aa74a1c731f856168545938
Robert Pohlink <pohlinkzei@gmx-topmail.de>
2015-08-26 12:57:58 +0200

    add license

--- dog_display.h
+++ dog_display.h
@@ -1,3 +1,10 @@
+/* ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE"
+ * pohlinkzei@gmx-topmail.de wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Robert Pohlink
+ * ----------------------------------------------------------------------------*/
+
 #ifndef DOG_DISPLAY_H
 #define DOG_DISPLAY_H
 /** INCLUDES *********************************************************************************************************************/
